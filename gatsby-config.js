1769ef8d098110e0a48a4b6aac8d3d147651c73d
jncd-v <jncdev@protonmail.com>
2022-01-04 23:42:08 +0100

    added start_url / to gatsby manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,6 +15,7 @@ module.exports = {
       resolve: "gatsby-plugin-manifest",
       options: {
         icon: "src/images/favicon.png",
+        start_url: `/`,
       },
     },
     "gatsby-transformer-remark",
